1f14535b5218e161a0bf48edba5a92feffbc2766
Claudio Cambra <developer@claudiocambra.com>
2023-01-09 01:57:33 +0100

    Use constant strings to define column names in users table
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- internal/db/usersdata.go
+++ internal/db/usersdata.go
@@ -2,6 +2,16 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+)
+
+const (
+	usersTableName    = "users"
+	idColumn          = "id"
+	displayNameColumn = "displayname"
+	userNameColumn    = "username"
+	keyColumn         = "key"
+	saltColumn        = "salt"
 )
 
 type UsersData struct {
@@ -9,15 +19,20 @@ type UsersData struct {
 }
 
 func (usersData *UsersData) Migrate() error {
-	query := `
-        CREATE TABLE IF NOT EXISTS users (
-            id          INTEGER     PRIMARY KEY AUTOINCREMENT,
-            displayname TEXT,
-            username    TEXT        NOT NULL UNIQUE,
-            key         TEXT        NOT NULL,
-            salt        TEXT        NOT NULL
+	query := fmt.Sprintf(`
+        CREATE TABLE IF NOT EXISTS %s (
+            %s  INTEGER     PRIMARY KEY AUTOINCREMENT,
+            %s  TEXT,
+            %s  TEXT        NOT NULL UNIQUE,
+            %s  TEXT        NOT NULL,
+            %s  TEXT        NOT NULL
         );
-    `
+    `, usersTableName,
+		idColumn,
+		displayNameColumn,
+		userNameColumn,
+		keyColumn,
+		saltColumn)
 
 	_, err := usersData.db.Exec(query)
 	return err
