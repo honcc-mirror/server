e91699afd838593c629f1a795d83983bb5bc879f
Claudio Cambra <developer@claudiocambra.com>
2023-01-10 21:57:51 +0100

    Move database setup for usersdata_test to separate func
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- internal/db/usersdata_test.go
+++ internal/db/usersdata_test.go
@@ -13,23 +13,29 @@ const (
 	testDbFileName = "usersdata_test.db"
 )
 
-func cleanupDb() {
+func setupTestDb(test *testing.T) *sql.DB {
+	test.Cleanup(cleanupTestDb)
+
+	db, err := sql.Open("sqlite3", testDbFileName)
+	if err != nil {
+		test.Fatalf("Could not create test database: %s Received error: %s", testDbFileName, err)
+	}
+
+	return db
+}
+
+func cleanupTestDb() {
 	err := os.Remove(testDbFileName)
 	if err != nil {
 		log.Println("Could not delete test db file!", err)
 	}
 }
 
-func TestMigration(t *testing.T) {
-	t.Cleanup(cleanupDb)
-	db, err := sql.Open("sqlite3", testDbFileName)
-	if err != nil {
-		t.Fatalf("Could not create test database: %s Received error: %s", testDbFileName, err)
-	}
-
+func TestMigration(test *testing.T) {
+	db := setupTestDb(test)
 	usersData := UsersData{db: db}
 	migrateErr := usersData.Migrate()
 	if migrateErr != nil {
-		t.Fatalf("Could not migrate test database. Received error: %s", migrateErr)
+		test.Fatalf("Could not migrate test database. Received error: %s", migrateErr)
 	}
 }
