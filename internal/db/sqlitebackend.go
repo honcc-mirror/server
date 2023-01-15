f4d0c88d41d3d159a079944a0fb8e15d72ffb7cf
Claudio Cambra <developer@claudiocambra.com>
2023-01-15 01:49:23 +0100

    Do not enforce default location as DB open location
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- internal/db/sqlitebackend.go
+++ internal/db/sqlitebackend.go
@@ -2,28 +2,29 @@ package db
 
 import (
 	"database/sql"
-	"honcc/server/internal/locations"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type SQLiteBackend struct {
-	db *sql.DB
+	db       *sql.DB
+	location string
 }
 
 func (sqlb *SQLiteBackend) DB() *sql.DB {
 	return sqlb.db
 }
 
-func (sqlb *SQLiteBackend) Open() error {
-	db, err := sql.Open("sqlite3", locations.Get(locations.Database))
+func (sqlb *SQLiteBackend) Open(path string) error {
+	db, err := sql.Open("sqlite3", path)
 	if err != nil {
 		log.Fatalf("Could not open sqlite database: %s", err)
 		return err
 	}
 
 	sqlb.db = db
+	sqlb.location = path
 
 	return err
 }
--- internal/db/sqlitebackend_test.go
+++ internal/db/sqlitebackend_test.go
@@ -1,12 +1,25 @@
 package db
 
 import (
+	"log"
+	"os"
 	"testing"
 )
 
+const testDbFileName = "testdb.db"
+
+func cleanupTestDb() {
+	err := os.Remove(testDbFileName)
+	if err != nil {
+		log.Println("Could not delete test db file!", err)
+	}
+}
+
 func TestSQLiteBackendOpen(t *testing.T) {
+	t.Cleanup(cleanupTestDb)
+
 	backend := SQLiteBackend{}
-	err := backend.Open()
+	err := backend.Open(testDbFileName)
 	if err != nil {
 		t.Fatalf("Could not open sqlite db, received error: %s", err)
 	}
