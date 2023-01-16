ea782d51c51d5060b46110cdabb4d17cc64c12bf
Claudio Cambra <developer@claudiocambra.com>
2023-01-16 01:31:08 +0100

    Make most account fields a custom map rather than hard struct fields for flexibility
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- pkg/accounts/account.go
+++ pkg/accounts/account.go
@@ -1,8 +1,20 @@
 package accounts
 
-type PlatformAccount struct {
-	ID          string
-	Displayname string
-	Username    string
-	Password    string
+type AccountFieldTypeEnum string
+
+const (
+	TextField     AccountFieldTypeEnum = "text"
+	EmailField    AccountFieldTypeEnum = "email"
+	PasswordField AccountFieldTypeEnum = "password"
+	NumberField   AccountFieldTypeEnum = "number"
+)
+
+type AccountFieldName string
+type AccountFields map[AccountFieldName]AccountFieldTypeEnum
+type AccountData map[AccountFieldName]string
+
+type Account struct {
+	ID           string
+	Displayname  string
+	CustomFields AccountFields
 }
